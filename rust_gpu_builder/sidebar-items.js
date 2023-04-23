1cd40bae393b73e639b45b0d85e9c5db0b41cf06
Shfty <Shfty@users.noreply.github.com>
2023-04-23 00:59:06 +0000

    deploy: e68c2bbb0356b8d4a4695d6bfc5ef57c67543736

--- rust_gpu_builder/sidebar-items.js
+++ rust_gpu_builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["Msg",""]],"fn":[["async_watch","Watch a file or directory, sending relevant events through the provided channel."],["async_watcher","Instantiate an async watcher and return it alongside a channel to receive events on."],["handle_compile_result",""],["main",""]],"struct":[["ShaderBuilder","Clap application struct."]]};
+window.SIDEBAR_ITEMS = {"enum":["Msg","OutputFormat"],"fn":["async_watch","async_watcher","handle_compile_result","main"],"opaque":[""],"static":["CALLSITE","META"],"struct":["ShaderBuilder"]};
